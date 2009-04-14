68b45d09d36c0054d8a2eaf95b9bfdfa550877d1
Jozef Kutej <jozef@emo.cle.sk>
2009-04-14 22:31:26 +0200

    initial

---
+++ lib/File/is.pm
@@ -0,0 +1,138 @@
+package File::is;
+
+=head1 NAME
+
+File::is - file is older? is newer?
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+use Carp;
+use File::stat;
+
+=head1 SYNOPSIS
+
+    use File::is;
+
+    # return if F<file1> is newer than F<file2> or F<file3>.
+    return
+        if File::is->newer('file1', 'file2', 'file3');
+
+    # do something if F<path1/file1> is older than F<file2> or F<path3/file3>.
+    do_some_work()
+        if File::is->older([ 'path1', 'file1'], 'file2', [ 'path3', 'file3' ]);
+
+=head1 DESCRIPTION
+
+A portable (hopefully) way to check if file is older or newer than other files.
+
+=head1 FUNCTIONS
+
+=cut
+
+sub _cmp_mtime {
+    my $class    = shift;
+    my $cmp_func = shift;
+    my $file1 = _construct_filename(shift);
+    my @files = @_;
+    
+    my $file1_stat = stat($file1);
+    croak 'file "'.$file1.'" not reachable'
+        if not $file1_stat;
+    my $file1_mtime = $file1_stat->mtime;
+    
+    foreach my $file (@files) {
+        $file = _construct_filename($file);
+        my $file_stat = stat($file);
+        croak 'file "'.$file.'" not reachable'
+            if not $file_stat;
+        
+        # return success if condition is met
+        return 1
+            if $cmp_func->($file1_mtime, $file_stat->mtime);
+    }
+    
+    # no file was newer
+    return;
+}
+
+sub newer {
+    return shift->_cmp_mtime(sub { $_[0] > $_[1] }, @_);
+}
+
+sub older {
+    return shift->_cmp_mtime(sub { $_[0] < $_[1] }, @_);
+}
+
+sub _construct_filename {
+    croak 'need at least one argument'
+        if @_ == 0;
+    
+    return File::Spec->catfile(@{$_[0]})
+        if (@_ == 1) and (ref $_[0] eq 'ARRAY');
+    
+    return File::Spec->catfile(@_);
+}
+
+=head1 AUTHOR
+
+Jozef Kutej, C<< <jkutej at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-is at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-is>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::is
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-is>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-is>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-is>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-is>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Jozef Kutej, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+'sleeeeeeeeeeep';
---
+++ t/01_File-is.t
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+#use Test::More 'no_plan';
+use Test::More tests => 12;
+use Test::Differences;
+use Test::Exception;
+
+use File::Temp 'tempdir';
+use File::Path 'mkpath';
+
+BEGIN {
+    use_ok ( 'File::is' ) or exit;
+}
+
+exit main();
+
+sub main {
+	# test _construct_filename()
+	dies_ok { File::is::_construct_filename(); } 'die when no arg to _construct_filename()';
+	is(
+		File::is::_construct_filename('file'),
+		'file',
+		'_construct_filename() with one argument'
+	);
+	is(
+		File::is::_construct_filename('folder', 'subfolder', 'file'),
+		File::Spec->catfile('folder', 'subfolder', 'file'),
+		'_construct_filename() with more argument'
+	);
+	is(
+		File::is::_construct_filename([ 'folder', 'subfolder', 'file' ]),
+		File::Spec->catfile('folder', 'subfolder', 'file'),
+		'_construct_filename() with array ref argument'
+	);
+	
+	# setup three files in different folders
+	my $tfolder = tempdir( CLEANUP => 1 );
+	my $tfolder1 = File::Spec->catdir($tfolder, 'sub', 'sub');
+	mkpath($tfolder1);
+	my $tfolder2 = File::Spec->catdir($tfolder, 'sub1', 'sub2');
+	mkpath($tfolder2);
+	my $tfolder3 = File::Spec->catdir($tfolder, 'sub3', 'sub4');
+	mkpath($tfolder3);
+	my $fn1 = File::Spec->catfile($tfolder1, 'f1');
+	my $fn2 = File::Spec->catfile($tfolder2, 'f2');
+	my $fn3 = File::Spec->catfile($tfolder3, 'f3');
+	diag 'three files ('.$fn1.', '.$fn2.', '.$fn3.')';
+	open my $fh1, '>', $fn1;
+	open my $fh2, '>', $fn2;
+	open my $fh3, '>', $fn3;
+	
+	# test newer
+	ok(!File::is->newer($fn1, $fn2, $fn3), 'file1 was created as first cannot be newer');
+	utime time(), time()-10, $fn2;
+	ok(!File::is->newer($fn1, $fn3), 'file1 still not never then file3');
+	ok(!File::is->newer($fn1, [ $tfolder, 'sub3', 'sub4', 'f3' ]), 'file1 still not never then file3');
+	ok(File::is->newer($fn1, $fn2), 'but newer than file2 ( time()-10 )');
+	ok(File::is->newer([ $tfolder, 'sub', 'sub', '..', 'sub', 'f1' ], [ $tfolder, 'sub1', 'sub2', 'f2' ]), 'but newer than file2 ( time()-10 )');
+
+	# test older
+	ok(File::is->older($fn2, $fn1), 'file2 is older then file1');
+	ok(!File::is->older($fn3, $fn1), 'file3 was create last can not be older than file1');
+	
+	return 0;
+}
+
